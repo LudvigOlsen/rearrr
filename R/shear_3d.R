421c0bbedc11ad7fa6d2356089e70d7ef7519a20
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2020-10-16 12:45:19 +0200

    Work on shear_3d. Now allows multiple shearing amounts

--- R/shear_3d.R
+++ R/shear_3d.R
@@ -11,15 +11,45 @@
 #' @description
 #'  \Sexpr[results=rd, stage=render]{lifecycle::badge("experimental")}
 #'
-#'  TODO
+#'  Shear a set of 3d points around an origin.
+#'
+#'  Applies one of the following transformation matrices, depending on which
+#'  two shearing amounts are specified:
+#'
+#'  Given \code{`x_shear`} and \code{`y_shear`}:
+#'
+#'  | [ \eqn{1} |, \eqn{0} |, \code{x_shear} | ] |
+#'  | :--- | :--- | :--- | :--- |
+#'  | [ \eqn{0} |, \eqn{1} |, \code{y_shear} | ] |
+#'  | [ \eqn{0} |, \eqn{0} |, \eqn{1} | ] |
+#'
+#'  Given \code{`x_shear`} and \code{`z_shear`}:
+#'
+#'  | [ \eqn{1} |, \code{x_shear} |, \eqn{0} | ] |
+#'  | :--- | :--- | :--- | :--- |
+#'  | [ \eqn{0} |, \eqn{1} |, \eqn{0} | ] |
+#'  | [ \eqn{0} |, \code{z_shear} |, \eqn{1} | ] |
+#'
+#'  Given \code{`y_shear`} and \code{`z_shear`}:
+#'
+#'  | [ \eqn{1} |, \eqn{0} |, \eqn{0} | ] |
+#'  | :--- | :--- | :--- | :--- |
+#'  | [ \code{y_shear} |, \eqn{1} |, \eqn{0} | ] |
+#'  | [ \code{z_shear} |, \eqn{0} |, \eqn{1} | ] |
+#'
+#'  The data points in \code{`data`} are moved prior to the shearing, to bring
+#'  the origin to \code{0} in all dimensions. After the shearing, the
+#'  inverse move is applied to bring the origin back to its original position.
 #'
 #'  The origin can be supplied as coordinates or as a function that returns coordinates. The
 #'  latter can be useful when supplying a grouped \code{data.frame} and shearing around e.g. the centroid
 #'  of each group.
 #' @author Ludvig Renbo Olsen, \email{r-pkgs@@ludvigolsen.dk}
-#' @param x_shear,y_shear,z_shear TODO
 #' @param x_col,y_col,z_col Name of x/y/z column in \code{`data`}. All must be specified.
-#' @param origin Coordinates of the origin to rotate around. Must be a \code{vector} with 3 elements (i.e. origin_x, origin_y, origin_z).
+#' @param x_shear,y_shear,z_shear Shear factor for the x/y/z dimension (\code{numeric}). Decides the amount of shearing.
+#'  Can be \code{vector}s with multiple shear factors.
+#' @param origin Coordinates of the origin to shear around.
+#'  \code{Vector} with 3 elements (i.e. origin_x, origin_y, origin_z).
 #'  Ignored when \code{`origin_fn`} is not \code{NULL}.
 #' @param shear_col_name Name of new column with the shearing amounts. If \code{NULL}, no column is added.
 #'
@@ -27,7 +57,7 @@
 #'  when plotting multiple shears.
 #' @param origin_col_name Name of new column with the origin coordinates. If \code{NULL}, no column is added.
 #' @keywords internal
-#' @return \code{data.frame} (\code{tibble}) with five new columns containing
+#' @return \code{data.frame} (\code{tibble}) with six new columns containing
 #'  the sheared x-, y- and z-values and the shearing amounts and origin coordinates.
 #' @details
 #'  TODO
@@ -47,7 +77,7 @@
 #'          y = Value) %>%
 #'   mutate(z = 1)
 #'
-#' # Shear the x variable with regards to y
+#' # Shear the x and z axes
 #' # around the centroid
 #' df_sheared <- shear_3d(
 #'   data = df_square,
@@ -62,7 +92,6 @@
 #' # Plot sheared data
 #' # Black: original points
 #' # Red: sheared points
-#' # Size: sheared z value
 #' df_sheared %>%
 #'   ggplot(aes(x = x, y = y)) +
 #'   geom_point() +
@@ -81,7 +110,68 @@
 #' )
 #' }
 #'
+#' # Shear the y and z axes
+#' # around the centroid
+#' df_sheared <- shear_3d(
+#'   data = df_square,
+#'   x_col = "x",
+#'   y_col = "y",
+#'   z_col = "z",
+#'   y_shear = 2,
+#'   z_shear = 4,
+#'   origin_fn = centroid
+#' )
 #'
+#' # Plot sheared data
+#' # Black: original points
+#' # Red: sheared points
+#' df_sheared %>%
+#'   ggplot(aes(x = x, y = y)) +
+#'   geom_point() +
+#'   geom_point(aes(x = x_sheared, y = y_sheared, color = "red")) +
+#'   theme_minimal()
+#'
+#' \dontrun{
+#' # Plot 3d with plotly
+#' plotly::plot_ly(
+#'   x = df_sheared$x_sheared,
+#'   y = df_sheared$y_sheared,
+#'   z = df_sheared$z_sheared,
+#'   type = "scatter3d",
+#'   mode = "markers",
+#'   color = df_sheared$.shear_str
+#' )
+#' }
+#'
+#' # Shear the y and z axes with multiple amounts at once
+#' # around the centroid
+#' df_sheared <- shear_3d(
+#'   data = df_square,
+#'   x_col = "x",
+#'   y_col = "y",
+#'   z_col = "z",
+#'   y_shear = c(0, 2, 4),
+#'   z_shear = c(0, 4, 6),
+#'   origin_fn = centroid
+#' )
+#'
+#' # Plot sheared data
+#' df_sheared %>%
+#'   ggplot(aes(x = x_sheared, y = y_sheared, color = .shear_str)) +
+#'   geom_point() +
+#'   theme_minimal()
+#'
+#' \dontrun{
+#' # Plot 3d with plotly
+#' plotly::plot_ly(
+#'   x = df_sheared$x_sheared,
+#'   y = df_sheared$y_sheared,
+#'   z = df_sheared$z_sheared,
+#'   type = "scatter3d",
+#'   mode = "markers",
+#'   color = df_sheared$.shear_str
+#' )
+#' }
 #' }
 shear_3d <- function(data,
                      x_col,
@@ -105,21 +195,21 @@ shear_3d <- function(data,
     x_shear,
     any.missing = FALSE,
     null.ok = TRUE,
-    len = 1,
+    min.len = 1,
     add = assert_collection
   )
   checkmate::assert_numeric(
     y_shear,
     any.missing = FALSE,
     null.ok = TRUE,
-    len = 1,
+    min.len = 1,
     add = assert_collection
   )
   checkmate::assert_numeric(
     z_shear,
     any.missing = FALSE,
     null.ok = TRUE,
-    len = 1,
+    min.len = 1,
     add = assert_collection
   )
   checkmate::assert_string(x_col, add = assert_collection)
@@ -144,35 +234,80 @@ shear_3d <- function(data,
     unique = TRUE,
     add = assert_collection
   )
-  if (length(c(x_shear, y_shear, z_shear)) != 2){
+  checkmate::reportAssertions(assert_collection)
+  shear_lengths <- c(length(x_shear), length(x_shear), length(z_shear))
+  if (length(shear_lengths) != 2){
     assert_collection$push(
       "Exactly 2 of {x_shear, y_shear, z_shear} must be non-null."
     )
   }
-  checkmate::reportAssertions(assert_collection)
+  if (shear_lengths[[1]] > 1 && shear_lengths[[2]] > 2 &&
+      shear_lengths[[1]] != shear_lengths[[2]]){
+    assert_collection$push("when both shear factors have length > 1, they must have the same length.")
+  }
   # Check if we will need to overwrite columns
   check_unique_colnames_(x_col, y_col, z_col, shear_col_name, origin_col_name)
   check_overwrite_(data = data, nm = shear_col_name, overwrite = overwrite)
   check_overwrite_(data = data, nm = origin_col_name, overwrite = overwrite)
   # End of argument checks ####
 
-  multi_mutator_(
-    data = data,
-    mutate_fn = shear_3d_mutator_method_,
-    check_fn = NULL,
-    force_df = TRUE,
-    overwrite = overwrite,
-    min_dims = 3,
-    keep_original = keep_original,
-    cols = c(x_col, y_col, z_col),
-    x_shear = x_shear,
-    y_shear = y_shear,
-    z_shear = z_shear,
-    suffix = suffix,
-    origin = origin,
-    origin_fn = origin_fn,
-    origin_col_name = origin_col_name,
-    shear_col_name = shear_col_name
+  if (!is.null(x_shear))
+    num_shears <- length(x_shear)
+  else
+    num_shears <- length(y_shear)
+
+  # Define function for getting shearing factor
+  # when it is specified
+  # When `shear` has a single element,
+  # we get that no matter the given index
+  get_shear_ <- function(shear, idx){
+    if (!is.null(shear)){
+      if (length(shear) == 1)
+        idx <- 1
+      shear[[idx]]
+    }
+  }
+
+  purrr::map_dfr(
+    .x = seq_len(num_shears),
+    .f = function(shear_idx) {
+      # Get current shearing factors
+      curr_x_shear <- get_shear_(x_shear, idx = shear_idx)
+      curr_y_shear <- get_shear_(y_shear, idx = shear_idx)
+      curr_z_shear <- get_shear_(z_shear, idx = shear_idx)
+
+      # Shear the data
+      out <- multi_mutator_(
+        data = data,
+        mutate_fn = shear_3d_mutator_method_,
+        check_fn = NULL,
+        force_df = TRUE,
+        overwrite = overwrite,
+        min_dims = 3,
+        keep_original = keep_original,
+        cols = c(x_col, y_col, z_col),
+        x_shear = curr_x_shear,
+        y_shear = curr_y_shear,
+        z_shear = curr_z_shear,
+        suffix = suffix,
+        origin = origin,
+        origin_fn = origin_fn,
+        origin_col_name = origin_col_name
+      )
+
+      # Add info columns
+      if (!is.null(shear_col_name)) {
+        shear_amounts <- list(curr_x_shear, curr_y_shear, curr_z_shear)
+        shear_amounts <-
+          replace(shear_amounts, shear_amounts == "NULL", NA_real_)
+        out[[shear_col_name]] <-
+          list_coordinates_(shear_amounts, names = c(x_col, y_col, z_col))
+        out <-
+          paste_coordinates_column_(out, shear_col_name, na.rm = TRUE)
+      }
+
+      out
+    }
   )
 }
 
@@ -188,7 +323,6 @@ shear_3d_mutator_method_ <- function(data,
                                      origin,
                                      origin_fn,
                                      origin_col_name,
-                                     shear_col_name,
                                      ...) {
 
   # Create rotation matrix
@@ -234,12 +368,6 @@ shear_3d_mutator_method_ <- function(data,
   )
 
   # Add info columns
-  if (!is.null(shear_col_name)) {
-    shear_amounts <- list(x_shear, y_shear, z_shear)
-    shear_amounts <- replace(shear_amounts, shear_amounts == "NULL", NA_real_)
-    data[[shear_col_name]] <- list_coordinates_(shear_amounts, names = cols)
-    data <- paste_coordinates_column_(data, shear_col_name, na.rm = TRUE)
-  }
   if (!is.null(origin_col_name)) {
     data[[origin_col_name]] <- list_coordinates_(origin, names = cols)
   }
